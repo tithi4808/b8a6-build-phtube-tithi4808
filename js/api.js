b1d1575e15d404d11df7c802815ef9c875598634
tithi2149 <tanyasultana60@gmail.com>
2023-09-05 23:20:11 +0600

    add

--- js/api.js
+++ js/api.js
@@ -30,24 +30,27 @@ const handleid=async(category_id)=>
 {
   const res=await fetch(` https://openapi.programming-hero.com/api/videos/category/${category_id}`)
  const data=await res.json()
+ console.log(data)
+
  const card=document.getElementById('card')
-card.innerHTML=''
+ card.innerHTML=''
+ const notfound=document.getElementById('no_found');
+  notfound.innerHTML=''
  if(data.status===true)
- {
+{
   
 data.data.forEach((catagory)=>{
-  
+
 const div=document.createElement('div')
 div.innerHTML=`<div class="card w-80 h-80 mt-10 lg:mx-auto ">
 
-<figure><img class="relative" src="${catagory.thumbnail}" alt="videos" /><p class="   absolute bg-black text-white right-1 bottom-36" id="time" ><span>${((catagory.others.posted_date)-(catagory.others.posted_date%3600))/3600} hrs </span><span>${((catagory.others.posted_date%3600)-(catagory.others.posted_date%60))/60
-}min ago</span></p></figure>
+<figure><img class="relative" src="${catagory.thumbnail}" alt="videos" /><p class="   absolute bg-black text-white right-1 bottom-36" id="time" ><span>${time(catagory.others.posted_date)} </span><span>${minutes(catagory.others.posted_date)
+}</span></p></figure>
 
 <div class="card-body">
 <div class="flex gap-2 ">
 <img class="w-10 h-10 rounded-full" src="${catagory.authors.map((pp)=>pp.profile_picture)}" alt="">
 
-  
   
   <div>
     <h2 class="card-title">${catagory.title}
@@ -62,19 +65,16 @@ div.innerHTML=`<div class="card w-80 h-80 mt-10 lg:mx-auto ">
 card.appendChild(div)
 
 
-})
-
- }
- else {
-
-  drawing('1005')
- }
-
- 
-
 
   
- 
+
+
+})
+
+}
+ else{
+  drawing('1005')
+}
 }
 
 
@@ -87,16 +87,21 @@ const drawing=async(id)=>
   
   
 
-  const notfound=document.getElementById('no_found')
+  const notfound=document.getElementById('no_found');
   notfound.innerHTML=''
+  
+
   const div=document.createElement("div")
+  
   div.innerHTML=`
-    <div class="mt-20 text-center text-2xl"><img class="flex lg:mx-40" src="Icon.png" />
+    <img class="flex lg:mx-40" src="Icon.png" />
     <p class="mt-4">Oops!! Sorry, There is no</p>
-    <p class="mt-4 ">content here</p></div>
+    <p class="mt-4 ">content here</p>
 
   `
   notfound.appendChild(div)
+  
+  
 
 
 }
@@ -104,7 +109,38 @@ const drawing=async(id)=>
 
 
 
+const minutes=(num)=>
+{
+  
+  if(num>=60)
+  {
+    const n=(((num%3600)-(num%60))/60)+'min ago'
+    return n
+    
+  }
+  else{
+    const n=''
+    return n
+
+  }
+ 
+}
+const time=(num)=>
+{
+  
+  if(num>=60)
+  {
+    const n=((num-(num%3600))/3600)+'hrs'
+    return n
+    
+  }
+  else{
+    const n=''
+    return n
+
+  }
+ 
+};
+
 loadvideos();
-handleid(1000)
-
-
+handleid(1000);
